b8a2ed7e77b4bdea036e7f008fd511d203471d19
Yashaswee Kesharwani <yashaswee.email@gmail.com>
2024-05-20 18:35:28 +0530

    added global error handler

--- server.ts
+++ server.ts
@@ -1,6 +1,8 @@
+import { config } from './src/config/config';
 import app from './src/app'
+
 const startSever = () => {
-    const port = process.env.PORT || 3000;
+    const port = config.port || 3000;
     app.listen(port,()=>{
         console.log('Listening to Port:',port);
     });
