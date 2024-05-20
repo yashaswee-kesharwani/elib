a47b2fca8f204cd7e64fab3d1246e766e23701ec
Yashaswee Kesharwani <yashaswee.email@gmail.com>
2024-05-20 19:33:47 +0530

    created user router

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response, NextFunction } from 'express'
 import createHttpError, { HttpError } from 'http-errors';
 import { config } from './config/config';
-import globalErrorHandler from './middlewares/globalerrorhandler';
+import globalErrorHandler from './middlewares/globalErrorHandler';
+import userRouter from './user/userRouter';
 
 const app = express()
 app.get("/",(req,res,next)=> {
@@ -10,8 +11,10 @@ app.get("/",(req,res,next)=> {
     res.json({"message":"Welcome to elib apis"});
 });
 
+//Setting up Router
+app.use("/api/users",userRouter);
+
 // Global Error Handler
 app.use(globalErrorHandler)
-
 
 export default app
---
+++ src/user/userController.ts
@@ -0,0 +1,7 @@
+import { Response, Request, NextFunction } from 'express'
+const createUser = async (req:Request,res:Response,next:NextFunction)=>{
+    res.json({
+        message:"User Created"
+    });
+};
+export {createUser};
