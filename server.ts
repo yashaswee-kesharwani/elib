5c27acee88e7ed11e44b86e3d67db61cf6640895
Yashaswee Kesharwani <yashaswee.email@gmail.com>
2024-05-20 11:35:59 +0530

    added config

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import app from './src/app'
 const startSever = () => {
     const port = process.env.PORT || 3000;
     app.listen(port,()=>{
-        console.log('Listening to Port: ${port}');
+        console.log('Listening to Port:',port);
     });
 }
 
