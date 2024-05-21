1dc6d9d8c5cb59b1a7e3cbdde70a6895ed99dcc6
Yashaswee Kesharwani <yashaswee.email@gmail.com>
2024-05-21 08:36:13 +0530

    add system to upload on mongo

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,38 +1,53 @@
-import path from 'node:path';
+import path from "node:path";
 import express, { Request, Response, NextFunction } from "express";
 import cloudinary from "../config/cloudinary";
+import bookModel from "./bookModel";
+import fs from "node:fs";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
-	const files = req.files as {[filename:string]:Express.Multer.File[]}; // Remember this line
-	const coverImageMimeType = files.coverImage[0].mimetype.split('/').at(-1);
+	const { title, genre } = req.body;
+	const files = req.files as { [filename: string]: Express.Multer.File[] }; // Remember this line
+	const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
 	const fileName = files.coverImage[0].filename;
-	const filePath = path.resolve(__dirname,'../../public/data/uploads',fileName)
-	const uploadResult = await cloudinary.uploader.upload(
-		filePath,
-		{
-			filename_override: fileName,
-			folder: 'book-covers',
-			format: coverImageMimeType
-		}
+	const filePath = path.resolve(
+		__dirname,
+		"../../public/data/uploads",
+		fileName
 	);
-	console.log(uploadResult);
-	
-	const bookMimeType = files.file[0].mimetype.split('/').at(-1);
-	const bookName = files.file[0].filename;
-	const bookPath = path.resolve(__dirname,'../../public/data/uploads',bookName)
-	const bookUploadResult = await cloudinary.uploader.upload(
-		bookPath,
-		{
-			resource_type:'raw',
-			filename_override: fileName,
-			folder: 'book-pdfs',
-			format: bookMimeType
-		}
-	);
-	console.log(bookUploadResult);
-	
+	const uploadResult = await cloudinary.uploader.upload(filePath, {
+		filename_override: fileName,
+		folder: "book-covers",
+		format: coverImageMimeType,
+	});
 
-    
+	const bookMimeType = files.file[0].mimetype.split("/").at(-1);
+	const bookName = files.file[0].filename;
+	const bookPath = path.resolve(
+		__dirname,
+		"../../public/data/uploads",
+		bookName
+	);
+	const bookUploadResult = await cloudinary.uploader.upload(bookPath, {
+		resource_type: "raw",
+		filename_override: fileName,
+		folder: "book-pdfs",
+		format: bookMimeType,
+	});
+
+	const newBook = await bookModel.create({
+		title: title,
+		genre: genre,
+		author: "664ba3288254d131d4d1e593",
+		coverImage: uploadResult.secure_url,
+		file: bookUploadResult.secure_url,
+	});
+
+	await fs.promises.unlink(filePath);
+	await fs.promises.unlink(bookPath);
+
+	res.status(201).json({
+		id: newBook._id,
+	});
 };
 const getBook = async (req: Request, res: Response, next: NextFunction) => {
 	res.json({});
