fb1558a81951e3c86fe1b7f43cfc7b635f67d30a
Yashaswee Kesharwani <yashaswee.email@gmail.com>
2024-05-21 08:26:50 +0530

    upload files to cloudinary

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,9 +1,37 @@
+import path from 'node:path';
 import express, { Request, Response, NextFunction } from "express";
 import cloudinary from "../config/cloudinary";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
-	const uploadResult = await cloudinary.uploader.upload();
+	const files = req.files as {[filename:string]:Express.Multer.File[]}; // Remember this line
+	const coverImageMimeType = files.coverImage[0].mimetype.split('/').at(-1);
+	const fileName = files.coverImage[0].filename;
+	const filePath = path.resolve(__dirname,'../../public/data/uploads',fileName)
+	const uploadResult = await cloudinary.uploader.upload(
+		filePath,
+		{
+			filename_override: fileName,
+			folder: 'book-covers',
+			format: coverImageMimeType
+		}
+	);
+	console.log(uploadResult);
 	
+	const bookMimeType = files.file[0].mimetype.split('/').at(-1);
+	const bookName = files.file[0].filename;
+	const bookPath = path.resolve(__dirname,'../../public/data/uploads',bookName)
+	const bookUploadResult = await cloudinary.uploader.upload(
+		bookPath,
+		{
+			resource_type:'raw',
+			filename_override: fileName,
+			folder: 'book-pdfs',
+			format: bookMimeType
+		}
+	);
+	console.log(bookUploadResult);
+	
+
     
 };
 const getBook = async (req: Request, res: Response, next: NextFunction) => {
